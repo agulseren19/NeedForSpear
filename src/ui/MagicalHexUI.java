e94f1b7eaee02b107d343bd42bc562f678889c61
github_agulseren19 <agulseren19@ku.edu.tr>
2023-08-15 16:52:32 +0300

    NeedForSpear

---
+++ src/ui/MagicalHexUI.java
@@ -0,0 +1,37 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
+import domain.MagicalAbilityPackage.MagicalHex;
+
+public class MagicalHexUI extends JLabel {
+
+	public static ArrayList<MagicalHex> magicalHexDomainList;
+	public MagicalHex hexDomain;
+
+	public MagicalHexUI(MagicalHex hexDomain) {
+		this.hexDomain = hexDomain;
+
+		this.setOpaque(true);
+		this.setBackground(Color.gray);
+		this.setBounds(hexDomain.getxLocation(), hexDomain.getyLocation(), hexDomain.getRadius() * 2,
+				hexDomain.getRadius() * 2);
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+		this.setBounds(hexDomain.getxLocation(), hexDomain.getyLocation(), hexDomain.getRadius() * 2,
+				hexDomain.getRadius() * 2);
+		hexDomain.checkHitAndUpdateLocation();
+
+	}
+
+}
